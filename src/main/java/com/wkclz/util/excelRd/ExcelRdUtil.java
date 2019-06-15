ef590957ee93a03dbc1ca56324533df7b38e7a3f
wangkc <Wkc@92010903md>
2019-06-15 11:48:08 +0800

    指定错误类型识别

--- src/main/java/com/wkclz/util/excelRd/ExcelRdUtil.java
+++ src/main/java/com/wkclz/util/excelRd/ExcelRdUtil.java
@@ -1,18 +1,19 @@
 package com.wkclz.util.excelRd;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 
 
 public class ExcelRdUtil {
 
+    protected static Object getCellValue(Cell cell, ExcelRdTypeEnum type) {
 
-    protected static Object getCellValue(XSSFCell cell, ExcelRdTypeEnum type) {
         if (cell == null || "".equals(cell.toString().trim())) {
             return null;
         }
-        CellType cellType = cell.getCellTypeEnum();
+
+        // 可预测的类型
+        CellType cellType = cell.getCellType();
         if (type == ExcelRdTypeEnum.INTEGER && cellType == CellType.NUMERIC) {
             Double numeric = cell.getNumericCellValue();
             return numeric.intValue();
@@ -33,36 +34,31 @@ public class ExcelRdUtil {
         if (type == ExcelRdTypeEnum.STRING && cellType == CellType.STRING) {
             return cell.getStringCellValue();
         }
+
+        // 不可预测的类型
+        if (cellType == CellType.STRING) {
+            return cell.getStringCellValue();
+        }
+        if (cellType == CellType.NUMERIC) {
+            return cell.getNumericCellValue();
+        }
+        if (cellType == CellType.FORMULA) {
+            return cell.getNumericCellValue();
+        }
+        if (cellType == CellType.BOOLEAN) {
+            return cell.getBooleanCellValue();
+        }
+        if (cellType == CellType.ERROR) {
+            return cell.getErrorCellValue();
+        }
+        if (cellType == CellType.BLANK) {
+            return null;
+        }
+        if (cellType == CellType._NONE) {
+            return null;
+        }
+
         return cell.toString();
     }
-
-    protected static Object getCellValue(HSSFCell cell, ExcelRdTypeEnum type) {
-        if (cell == null || "".equals(cell.toString().trim())) {
-            return null;
-        }
-        CellType cellType = cell.getCellTypeEnum();
-        if (type == ExcelRdTypeEnum.INTEGER && cellType == CellType.NUMERIC) {
-            Double numeric = cell.getNumericCellValue();
-            return numeric.intValue();
-        }
-        if (type == ExcelRdTypeEnum.LONG && cellType == CellType.NUMERIC) {
-            Double numeric = cell.getNumericCellValue();
-            return numeric.longValue();
-        }
-        if (type == ExcelRdTypeEnum.DOUBLE && cellType == CellType.NUMERIC) {
-            return cell.getNumericCellValue();
-        }
-        if (type == ExcelRdTypeEnum.DATETIME && cellType == CellType.NUMERIC) {
-            return cell.getDateCellValue();
-        }
-        if (type == ExcelRdTypeEnum.DATE && cellType == CellType.NUMERIC) {
-            return new java.sql.Date(cell.getDateCellValue().getTime());
-        }
-        if (type == ExcelRdTypeEnum.STRING && cellType == CellType.STRING) {
-            return cell.getStringCellValue();
-        }
-        return cell.getStringCellValue();
-    }
-
 
 }
